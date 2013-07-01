3853c407213c1b8cdde4545f906d3d7382fe22d2
Barret Schloerke <schloerke@gmail.com>
2013-07-01 14:41:09 -0400

    Added try/exept around txIn, txOut, and block parsing/printing.  Also moved block file reading inside 'for_each_block' and not hard coded to file #1
    All errors will be printed and all work will not be lost.

--- transaction.py
+++ transaction.py
@@ -73,30 +73,47 @@ def dump_transaction(datadir, db_env, tx_id):
 def dump_all_transactions(datadir, db_env):
   """ Dump all transactions.
   """
-  blockfile = open(os.path.join(datadir, "blk%04d.dat"%(1,)), "rb")
-  block_datastream = BCDataStream()
-  block_datastream.map_file(blockfile, 0)
   def for_each_block(block_data):
-    block_datastream.seek_file(block_data['nBlockPos'])
-    data = parse_Block(block_datastream)
-    block_datetime = datetime.fromtimestamp(data['nTime'])
-    dt = "%d-%02d-%02d-%02d-%02d-%02d"%(block_datetime.year, block_datetime.month, block_datetime.day, block_datetime.hour, block_datetime.minute, block_datetime.second)
-    for txn in data['transactions']:
-      try:
-        for txIn in txn['txIn']:
-          if txIn['prevout_hash'] == "\x00"*32:
-            print 'in\t' + txn['hash'] + '\tcoinbase\t' + dt 
-          else:
-            pk = extract_public_key(txIn['scriptSig'])
-            print 'in\t' + txn['hash'] + '\t' + long_hex(txIn['prevout_hash'][::-1]) + '\t' + str(txIn['prevout_n']) + '\t' + pk + '\t' + dt 
-        index = 0
-        for txOut in txn['txOut']:
-          pk = extract_public_key(txOut['scriptPubKey'])
-          print 'out\t' + txn['hash'] + '\t' + str(index) + '\t' + pk + '\t' + str(txOut['value']/1.0e8) + '\t' + dt 
-          index += 1
-      except:
-        pass
+    try:
+      blockfile = open(os.path.join(datadir, "blk%04d.dat"%(block_data['nFile'],)), "rb")
+
+      ds = BCDataStream()
+      ds.map_file(blockfile, block_data['nBlockPos'])
+
+      data = parse_Block(ds)
+      block_datetime = datetime.fromtimestamp(data['nTime'])
+      dt = "%d-%02d-%02d-%02d-%02d-%02d"%(block_datetime.year, block_datetime.month, block_datetime.day, block_datetime.hour, block_datetime.minute, block_datetime.second)
+      for txn in data['transactions']:
+        try:
+          for txIn in txn['txIn']:
+            try:
+              if txIn['prevout_hash'] == "\x00"*32:
+                print 'in\t' + txn['hash'] + '\tcoinbase\t' + dt
+              else:
+                pk = extract_public_key(txIn['scriptSig'])
+                print 'in\t' + txn['hash'] + '\t' + long_hex(txIn['prevout_hash'][::-1]) + '\t' + str(txIn['prevout_n']) + '\t' + pk + '\t' + dt
+            except Exception, err:
+              print 'error_txIn\t' + str(block_data['nHeight']) + '\t' + str(err) + '\t' + str(txnIn) + '\t' + str(txn)
+          index = 0
+          for txOut in txn['txOut']:
+            try:
+              pk = extract_public_key(txOut['scriptPubKey'])
+              # txOutKeyDecoded = [ x for x in script_GetOp(bytes) ]
+              print 'out\t' + txn['hash'] + '\t' + str(index) + '\t' + pk + '\t' + str(txOut['value']/1.0e8) + '\t' + dt + '\t'# + txOutKeyDecoded
+            except Exception, err:
+              print 'error_txOut\t' + str(block_data['nHeight']) + '\t' + str(err) + '\t' + str(txOut) + '\t' + str(txn)
+
+            index += 1
+
+        except Exception, err:
+          print 'error\t' + str(block_data['nHeight']) + '\t' + str(err) + '\t' + str(txn)
+          pass
+    except Exception, err:
+        print 'error_block\t' + str(block_data['nHeight'])  + '\t' + str(err)
+
     return True
+
   scan_blocks(datadir, db_env, for_each_block)
   db_env.close()
+
 
